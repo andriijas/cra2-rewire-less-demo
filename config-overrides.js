45d0937d5060e8af8a293dfd2a3d6d27e5ef545f
Andreas Cederström <andriijas@users.noreply.github.com>
2018-01-22 11:38:41 +0100

    Initial commit

--- config-overrides.js
+++ config-overrides.js
@@ -3,7 +3,20 @@ const rewireLess = require("react-app-rewire-less-modules");
 //const rewireLess = require("react-app-rewire-less");
 
 module.exports = function override(config, env) {
-  const rewires = compose(rewireLess);
+  config = injectBabelPlugin(
+    ["import", { libraryName: "antd", style: true, libraryDirectory: "es" }],
+    config
+  );
+
+  const rewires = compose(
+    rewireLess.withLoaderOptions({
+      modifyVars: {
+        "@primary-color": "#DB7093",
+        "@info-color": "#DB7093",
+        "@layout-body-background": "#fff"
+      }
+    })
+  );
 
   return rewires(config, env);
 };
